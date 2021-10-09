68bdc0a9b02e680dbb743b9b1cc9a670595d1744
ruthrizzo <91860636+ruthrizzo@users.noreply.github.com>
2021-10-09 00:06:47 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,24 @@
 // Iteration 1: Names and Input
+let hacker1='Luis';
+console.log(`"The driver's name is ${hacker1}"`);
 
+
+let hacker2='Pedro';
+console.log(`"The navigator's name is ${hacker2}"`);
+
+//Iteration 2: Conditionals
+let str='Pedro';
+console.log(`"It seems that the navigator has the longest name, it has ${str.lenght} characters"`);
 
 // Iteration 2: Conditionals
-
+let str='Pedro';
+console.log(`"It seems that the navigator has the longest name, it has ${str.lenght} characters"`);
 
 // Iteration 3: Loops
+const sentence = "l u i s";
+console.log(sentence.toUpperCase());
+
+//3.2
+const Word="Pedro";
+let reverseWord=Word.split('').reverse().join('');
+console.log(reverseWord);
